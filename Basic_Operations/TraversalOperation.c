73d179c13434f74bf7e3dd04bc49508bb232dfb7
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-22 17:12:10 +0530

    first commit

---
+++ Basic_Operations/TraversalOperation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int arr[5] = {18,30,15,70,12};
+    int i;
+    printf("Elements of the array are:\n");
+    for(i=0;i<5;i++)
+    {
+       printf("Arr[%d]=%d,",i,arr[i]);
+    }
+}
