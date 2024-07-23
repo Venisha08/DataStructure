ff2902e528c89913106513becb581aa9f8746985
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-07-23 22:47:01 +0530

    2d array

---
+++ Array/TwodArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class TwodArray {
+    public static void main(String[] args) {
+        int [][]arr = new int[3][3];
+        Scanner sc = new Scanner(System.in);
+        for(int i =0; i<3;i++)
+        {
+            for(int j =0; j<3;j++)
+            {
+                System.out.println("Enter Element");
+                arr[i][j]=sc.nextInt();
+                System.out.println();
+            }
+        }
+        System.out.println("Printing Elements:");
+        for(int i =0;i<3;i++)
+        {
+            System.out.println();
+            for (int j =0;j<3;j++)
+            {
+                System.out.println(arr[i][j]+"\t");
+
+            }
+        }
+        }
+    }
