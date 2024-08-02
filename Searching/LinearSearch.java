51452e7f6624fa10ab62f5209650ee121ea9981b
Venisha08 <164355286+Venisha08@users.noreply.github.com>
2024-08-02 20:24:38 +0530

    Linear Search

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,25 @@
+package Searching;
+
+public class LinearSearch 
+{
+    private static void LinearSearch (int[]arr, int item )
+    {
+        for(int i =0;i<arr.length;i++)
+        {
+            if(arr[i] == item)
+            {
+                System.out.println(item + "found at index:"+i);
+                return;
+            }
+        }
+        System.out.println("Not found");
+    }
+
+    public static void main(String args[])
+    {
+        int [] arr = {12, 5, 18,25, -3, 19};
+        int item = 25;
+        LinearSearch(arr, item);
+        }
+    }
+
